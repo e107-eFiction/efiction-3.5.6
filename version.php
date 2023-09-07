8775231be8e57313380f04edc497c17260eff879
Jimako <janamicek@gmail.com>
2023-09-07 08:26:31 +0200

    fix mistypo - commented script backup

--- version.php
+++ version.php
@@ -7,7 +7,7 @@ $tmp = dbassoc(dbquery("SHOW COLUMNS FROM " . TABLEPREFIX . "fanfiction_stories 
 if($tmp['Type'] == "datetime") {
     dbquery("UPDATE " . TABLEPREFIX . "fanfiction_settings SET version = '3.5.5' WHERE sitekey = '" . SITEKEY . "'");
 }
-*/
+
 
 $tmp = dbassoc(dbquery("SHOW COLUMNS FROM " . TABLEPREFIX . "fanfiction_news LIKE 'time'"));
  
@@ -15,3 +15,5 @@ if ($tmp['Type'] == "datetime")
 {
     dbquery("UPDATE " . TABLEPREFIX . "fanfiction_settings SET version = '3.5.5' WHERE sitekey = '" . SITEKEY . "'");
 }
+
+*/
